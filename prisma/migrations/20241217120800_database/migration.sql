63266bfe4cec0904bd3bd60350f7d131ee74bdfc
Luis Henrique <luishenriquefonte223@gmail.com>
2025-04-07 10:35:21 -0300

    delete

--- prisma/migrations/20241217120800_database/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "PlacasCriadas" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "placa" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
